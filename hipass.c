d6ce4ade31cf6d1b5cd9d059c615ea9eeb9382a2
lknknm <sxswt@protonmail.com>
2023-04-06 14:50:51 -0300

    refactor(main): Refactor main() function

--- hipass.c
+++ hipass.c
@@ -6,7 +6,7 @@
 #include "lib/generator.h"
 #include "lib/clipboard.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
     printf("Hipass Password Generator\n\n");
 
@@ -15,14 +15,6 @@ int main(void)
     printf("Type in number of characters (between 14 and 128): ");
     scanf("%i", &characters);
     } while (characters < 14 || characters > 128);
-
-    srand(time(NULL));
-
-    char *digits = "0123456789";
-    char *lowercase = "abcdefghijklmnopqrstuvwxyz";
-    char *uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char *symbols = "!@#^&*$";
-    int symbols_length = strlen(symbols);
 
     char *password = malloc(sizeof(characters));
     if (password == NULL)
@@ -34,35 +26,6 @@ int main(void)
 
     printf("\033[0;32m"); // Green text value
     printf("Password: ");
-
-    // for (int i = 0; i < characters; i++)
-    // {
-    //     //char randomLetter = 'a' + rand() % 26;
-    //     //password[i] = randomLetter;
-
-    //     int char_type = rand() % 4;
-        
-    //     if (char_type == 0) {
-    //         password[i] = digits[rand() % 10];
-    //         printf("\033[0;34m"); //Blue text value
-    //         printf("%c", password[i]);
-    //     }
-    //     else if (char_type == 1) {
-    //         password[i] = lowercase[rand() % 26];
-    //         printf("\033[0;37m"); //White text value
-    //         printf("%c", password[i]);
-    //     }
-    //     else if (char_type == 2) {
-    //         password[i] = uppercase[rand() % 26];
-    //         printf("\033[0;37m"); //White text value
-    //         printf("%c", password[i]);
-    //     }
-    //     else if (char_type == 3) {
-    //         password[i] = symbols[rand() % symbols_length];
-    //         printf("\033[0;31m"); //Red text value
-    //         printf("%c", password[i]);
-    //     }
-    // }
 
     generate_random_complete(password, characters);
 
