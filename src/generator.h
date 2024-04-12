c489482677a3d9151a9b4f70a66c6a49be6cd5c6
lknknm <sxswt@protonmail.com>
2024-04-12 14:04:36 -0300

    General cleanup

--- src/generator.h
+++ src/generator.h
@@ -6,5 +6,4 @@
 // A-Z, a-z, symbols and numbers included.
 extern int generate_random_complete(void);
 extern int generate_random_CLI(bool CH_TYPE[], char *sufix, char *prefix);
-extern void generate_passphrase(void);
 #endif // GENERATOR_H
