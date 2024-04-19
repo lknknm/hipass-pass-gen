75b0aeb7cd767cbc33f0956edfb05b79e124a5d2
lknknm <github.snugness349@passinbox.com>
2024-04-19 01:12:17 -0300

    Update README excluding LastPass as recommendation

--- src/rand.cpp
+++ src/rand.cpp
@@ -10,7 +10,7 @@ extern "C"
 int random_nr(int min, int max) 
 {
 	std::uniform_int_distribution dist{min, max};
-	static std::mt19937           engine{randutils::auto_seed_128{}.base()};
+	static std::mt19937           engine{randutils::auto_seed_256{}.base()};
 	return dist(engine);
 }
 }
